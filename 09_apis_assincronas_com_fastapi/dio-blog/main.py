a01201bdfdc367bbfbfd7994ec4368916d56f9af
Alfredo Diani <alfredodiani@hotmail.com>
2026-04-23 00:25:15 -0300

    Rotas e endpoints: Request Body

--- 09_apis_assincronas_com_fastapi/dio-blog/main.py
+++ 09_apis_assincronas_com_fastapi/dio-blog/main.py
@@ -1,6 +1,7 @@
 from datetime import datetime, UTC
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -27,10 +28,21 @@ fake_db = [
     }
 ]
 
+#Classe para receber como parametro no POST
+class Post(BaseModel):
+    tittle: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False
 
+
+    
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    fake_db.append(post.model_dump())
+    return post
 
 @app.get('/posts')
-def readposts(skip: int = 0, limit: int = len(fake_db), published: bool = True):
+def readposts(limit: int, skip: int = 0, published: bool = True):
     return [post for post in fake_db[skip : skip + limit] if post['published'] is published]
 
 
