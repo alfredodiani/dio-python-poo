e8908ec8d4b0f18c54c857a7f750f486e05fed5d
Alfredo Diani <alfredodiani@hotmail.com>
2026-04-28 23:25:44 -0300

    Banco de dados: update

--- 10_banco_de_dados/conexao.py
+++ 10_banco_de_dados/conexao.py
@@ -6,21 +6,37 @@ ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meu_banco.sqlite")
 cursor = conexao.cursor()
 
-# cursor.execute("""
-#     CREATE TABLE clientes (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         nome VARCHAR(100),
-#         email VARCHAR(150)
-#     )
-#     """)
+def criar_tabela(conexao, cursor):
+    cursor.execute("""
+        CREATE TABLE clientes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR(100),
+            email VARCHAR(150)
+        )
+        """)
+    conexao.commit()
 
-# data = ("Alfredo", "alfredo@email.com")
-data = ("Lethicia", "lethicia@email.com")
 
-cursor.execute("""
-    INSERT INTO clientes (nome, email)
-        VALUES (?, ?)
-               
-""", data)
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute("""
+        INSERT INTO clientes (nome, email)
+        VALUES (?, ?);
+    """, data)
 
-conexao.commit()
+    conexao.commit()
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute("""
+        UPDATE clientes
+        SET nome = ?, email = ?
+        WHERE id = ?;
+    """, data)
+    conexao.commit()
+
+
+inserir_registro(conexao, cursor, "James None", "jnone@email.com")
+atualizar_registro(conexao, cursor, "John Doe", "johndoe@email.com", 1)
+atualizar_registro(conexao, cursor, "Jane Doe", "janedoe@email.com", 2)
+
