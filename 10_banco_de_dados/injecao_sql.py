26fd2fa4dce4cb79fe818704ca2849c2424d2328
Alfredo Diani <alfredodiani@hotmail.com>
2026-05-01 16:33:42 -0300

    Banco de dados: sql injection - prevenir

---
+++ 10_banco_de_dados/injecao_sql.py
@@ -0,0 +1,15 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.sqlite")
+cursor = conexao.cursor()
+cursor.row_factory = sqlite3.Row
+
+id_cliente = input("informe o id do cliente: ")
+
+cursor.execute(f"SELECT * FROM clientes WHERE id = {id_cliente}") # string  concatenada no sql pode ter problema com sql injection
+clientes = cursor.fetchall()
+for cliente in clientes:
+    print(dict(cliente))
