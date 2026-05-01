55847857b2f6129792d3f8dc301022f64dc3a81f
Alfredo Diani <alfredodiani@hotmail.com>
2026-05-01 18:05:06 -0300

    Manipulação de dados com FastAPI Assincrono: Conexão

---
+++ 09_apis_assincronas_com_fastapi/dio-blog/models/post.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from main import metadata
+
+posts = sa.Table(
+    'posts', 
+    metadata, 
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('title', sa.String(150), nullable=False, unique=True),
+    sa.Column('content', sa.String, nullable=False),
+    sa.Column('published_at', sa.DateTime, nullable=True),
+    sa.Column('publishe', sa.Boolean, default=False)
+)
